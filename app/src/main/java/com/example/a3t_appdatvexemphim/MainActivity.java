b117166d906fa717608b788a835700ef62d7113b
tanle16091998 <tanle16091998@gmail.com>
2024-10-11 01:14:52 +0700

    new update

--- app/src/main/java/com/example/a3t_appdatvexemphim/MainActivity.java
+++ app/src/main/java/com/example/a3t_appdatvexemphim/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.a3t_appdatvexemphim;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        signUp = findViewById(R.id.textViewSignUp);
+        imageView = findViewById(R.id.myImageView);
+        imageView.setBackgroundResource(R.drawable.animation_movie);
+        AnimationDrawable myAnim = (AnimationDrawable) imageView.getBackground();
+        myAnim.start();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        signUp.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, formSignIn.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
